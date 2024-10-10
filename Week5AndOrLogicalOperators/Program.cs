b8da5520b6457596562fe8a120d34f7a16bd9a22
Elisabeth Teearu <elisabeth282000@gmail.com>
2024-10-10 17:56:10 +0300

    week 5

---
+++ Week5AndOrLogicalOperators/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+
+
+
+
+
+string username, password;
+
+Console.WriteLine("Enter username:");
+username = Console.ReadLine();
+
+Console.WriteLine("Enter the password:");
+password = Console.ReadLine();
+
+if(username != "admin" || password  != "admin1234") //!= NOT
+{
+    Console.WriteLine("Invalid user credentials");
+}
+else
+{
+    Console.WriteLine("Welcome!");
+}
+
+
+
+//if (username == "admin" && password == "admin1234")
+//{
+//    Console.WriteLine("Welcome!");
+//}
+//else
+//{
+//    Console.WriteLine("Invalid user credentials");
+//}
